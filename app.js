b271671b7f44411141b486d825a78308e450cf86
ARJUN SHAJI <arjun.shaji@techgentsia.com>
2024-06-09 11:16:52 +0530

    -- basic fileupload

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const path = require("path");
 const session = require("express-session");
 const http = require("http");
 const connection = require("./connection");
+const { v4: uuidv4 } = require("uuid");
+var fileUpload = require("express-fileupload");
 
 const {
   defaultAdminEntrySQL,
   createAdminTableSQL,
   createTableSQL,
 } = require("./tables");
+const { log } = require("console");
 
 const port = process.env.PORT | 3000;
 const app = express();
@@ -38,6 +41,7 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(fileUpload());
 
 connection.connect(function (err) {
   if (err) throw err;
@@ -91,6 +95,65 @@ app.post("/login", (req, res) => {
     }
   );
   return;
+});
+
+app.post("/create", (req, res) => {
+  const {
+    firstname,
+    lastname,
+    email,
+    phone,
+    facebookid,
+    instagramid,
+    address,
+  } = req.body;
+
+  const imageId = uuidv4();
+  const questionId = uuidv4();
+  const profileImage = req.files && req.files.profile;
+  const questions = req.files && req.files.questions;
+
+  profileImage.mv("./public/image/" + imageId + ".jpg", (err, done) => {
+    // if (!err) {
+    //   res.render("admin/add-products");
+    // } else {
+    //   console.log(err);
+    // }
+  });
+
+  questions.mv("./public/excel/" + questionId + ".xlsx", (err, done) => {
+    // if (!err) {
+    //   res.render("admin/add-products");
+    // } else {
+    //   console.log(err);
+    // }
+  });
+
+  const id = uuidv4();
+
+  const insert = `
+    INSERT INTO student_details (id, firstname, lastname, address, email, phone, facebookid, instagramid, profileid, questionsid)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+  `;
+  connection.query(
+    insert,
+    [
+      id,
+      firstname,
+      lastname,
+      address,
+      email,
+      phone,
+      facebookid,
+      instagramid,
+      imageId,
+      questionId,
+    ],
+    (error, results, fields) => {
+      console.log("Student created successfully.");
+      res.render("admin/home", {});
+    }
+  );
 });
 
 const httpServer = http.createServer(app);
